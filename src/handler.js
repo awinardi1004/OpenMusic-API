ae7d1d7804e8c8623f40c8551e2c75fa7260ad95
awinardi1004 <awinardi1004@gmail.com>
2025-01-23 12:15:49 +0700

    create music handler sederhana

--- src/handler.js
+++ src/handler.js
@@ -8,7 +8,7 @@ const musics = require('./musics');
 const addAlbumsHandler = (request, h) => {
     const {name , year } = request.payload;
 
-    const id = nanoid(16);
+    const id = "album-" + nanoid(16);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
 
@@ -125,23 +125,136 @@ const deleteAlbumByIdHandler = (request, h) => {
 
 // musicHandler
 const addMusicHandler = (request, h) => {
-  const {title='untitled' , year } = request.payload;
+  const {title='untitled', year, genre, performer, duration, albumId} = request.payload;
 
     const id = nanoid(16);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
 
-    const newAlbum = {
-        id, name, year, createdAt, updatedAt,
+    const newMusic = {
+        id, title, year, performer, genre, duration, albumId, createdAt, updatedAt,
     };
 
-    albums.push(newAlbum);
-}
+    musics.push(newMusic);
+
+    const isSuccess = musics.filter((music) => music.id === id).length > 0;
+
+    if (isSuccess) {
+        const response = h.response({
+          status: 'success',
+          message: 'Menambahkan Music',
+          data: {
+            songId: id,
+          },
+        });
+        response.code(201);
+        return response;
+      }
+    
+      const response = h.response({
+        status: 'fail',
+        message: 'Music gagal ditambahkan',
+      });
+      response.code(500);
+      return response;
+};
+
+const getAllMusicHandler = () => ({
+  status: 'success',
+  data: {
+    musics,
+  },
+});
+
+const getMusicByIdHandler = (request, h) => {
+  const { id } = request.params;
+  const music = musics.filter((m) => m.id === id)[0];
+
+  if (music !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        music,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'music tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+const editMusicByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const { title, year, genre, performer, duration, albumId } = request.payload;
+  const updatedAt = new Date().toISOString();
+
+  const index = musics.findIndex((music) => music.id === id);
+
+  if (index !== -1) {
+    albums[index] = {
+      ...albums[index],
+      title,
+      year,
+      genre,
+      performer,
+      duration,
+      albumId,
+      updatedAt,
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'Music berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Music memperbarui album. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+const deleteMusicByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const index = musics.findIndex((music) => music.id === id);
+
+  if (index !== -1) {
+    musics.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'music berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Music gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
 
 module.exports = {
   addAlbumsHandler,
   getAllAlbumsHandler,
   getAlbumByIdHandler,
   editAlbumByIdHandler,
-  deleteAlbumByIdHandler
+  deleteAlbumByIdHandler,
+  addMusicHandler,
+  getAllMusicHandler,
+  getMusicByIdHandler,
+  editMusicByIdHandler,
+  deleteMusicByIdHandler
 };
