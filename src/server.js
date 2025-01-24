da1b34af36cb7df47abf75e72da7998d2ecabb0c
awinardi1004 <awinardi1004@gmail.com>
2025-01-24 10:20:55 +0700

    create album service

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const routes = [
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
   });
 
   server.route(routes);
