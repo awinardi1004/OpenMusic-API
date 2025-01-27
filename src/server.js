dd610048e7f6d89641156c76590de3135f4699c6
awinardi1004 <awinardi1004@gmail.com>
2025-01-27 08:03:02 +0700

    update handling error

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const albums = require('./api/albums');
 const songs = require('./api/songs');
@@ -13,8 +15,8 @@ const init = async () => {
   const albumsService = new AlbumsService();
 
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'], 
