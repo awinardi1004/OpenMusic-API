b3ed0b0990f6d9610f2b871fb61e146987bcefe2
awinardi1004 <awinardi1004@gmail.com>
2025-02-12 20:37:17 +0700

    protec resource playlists

--- src/api/playlists/routes.js
+++ src/api/playlists/routes.js
@@ -3,16 +3,25 @@ const routes = (handler) => [
         method: 'POST',
         path: '/playlists',
         handler: handler.postPlaylistHandler,
+        options: {
+          auth: 'playlists_jwt',
+        },
       },
       {
         method: 'GET',
         path: '/playlists',
         handler: handler.getPlaylistsHandler,
+        options: {
+          auth: 'playlists_jwt',
+        },
       },
       {
         method: 'DELETE',
         path: '/playlists/{id}',
         handler: handler.deletePlaylistByIdHandler,
+        options: {
+          auth: 'playlists_jwt',
+        },
       },
 ];
 
