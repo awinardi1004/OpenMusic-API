788c1dd1bc1b120f109c9dbe9efaa813a98b3940
awinardi1004 <awinardi1004@gmail.com>
2025-02-18 20:52:45 +0700

    updatae OpenMusic V2

--- migrations/1739448719965_add-foreign-key-to-owner-column.js
+++ migrations/1739448719965_add-foreign-key-to-owner-column.js
@@ -1,26 +1,17 @@
 exports.shorthands = undefined;
  
 exports.up = (pgm) => {
-  pgm.createTable('collaborations', {
-    id: {
-      type: 'VARCHAR(50)',
-      primaryKey: true,
-    },
-    playlist_id: {
-      type: 'VARCHAR(50)',
-      notNull: true,
-    },
-    user_id: {
-      type: 'VARCHAR(50)',
-      notNull: true,
-    },
-  });
+  pgm.sql("INSERT INTO users(id, username, password, fullname) VALUES ('old_playlists', 'old_playlists', 'old_playlists', 'old playlists')");
  
-  pgm.addConstraint('collaborations', 'unique_playlist_id_and_user_id', 'UNIQUE(playlist_id, user_id)');
-   pgm.addConstraint('collaborations', 'fk_collaborations.playlist_id_playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
-  pgm.addConstraint('collaborations', 'fk_collaborations.user_id_users.id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
+  pgm.sql("UPDATE playlists SET owner = 'old_playlists' WHERE owner IS NULL");
+ 
+  pgm.addConstraint('playlists', 'fk_playlists.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
 };
  
 exports.down = (pgm) => {
-  pgm.dropTable('collaborations');
+  pgm.dropConstraint('playlists', 'fk_playtlists.owner_users.id');
+ 
+  pgm.sql("UPDATE playlists SET owner = NULL WHERE owner = 'old_playlists'");
+ 
+  pgm.sql("DELETE FROM users WHERE id = 'old_playlists'");
 };
--- migrations/1739516308639_create-collaborations-table.js
+++ migrations/1739516308639_create-collaborations-table.js
@@ -1,6 +1,5 @@
- 
 exports.shorthands = undefined;
- 
+
 exports.up = (pgm) => {
   pgm.createTable('collaborations', {
     id: {
@@ -15,12 +14,13 @@ exports.up = (pgm) => {
       type: 'VARCHAR(50)',
       notNull: true,
     },
-  });
+  }, { ifNotExists: true });
+
   pgm.addConstraint('collaborations', 'unique_playlist_id_and_user_id', 'UNIQUE(playlist_id, user_id)');
-  pgm.addConstraint('collaborations', 'fk_collaborations.playlist_id_playlist.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
+  pgm.addConstraint('collaborations', 'fk_collaborations.playlist_id_playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
   pgm.addConstraint('collaborations', 'fk_collaborations.user_id_users.id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
 };
- 
+
 exports.down = (pgm) => {
   pgm.dropTable('collaborations');
 };
